150efa67e3da467c7959fd1ab5e0fea32e4917fa
Bonse <84657555+lxthxrgx@users.noreply.github.com>
2023-06-17 12:54:41 +0300

    Add files via upload

---
+++ data_base.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE IF NOT EXISTS public.cadnum
+(
+    cad text COLLATE pg_catalog."default",
+    category text COLLATE pg_catalog."default",
+    area text COLLATE pg_catalog."default",
+    unit_area text COLLATE pg_catalog."default",
+    koatuu text COLLATE pg_catalog."default",
+    use text COLLATE pg_catalog."default",
+    purpose text COLLATE pg_catalog."default",
+    purpose_code text COLLATE pg_catalog."default",
+    ownership text COLLATE pg_catalog."default",
+    ownershipcode text COLLATE pg_catalog."default",
+    geometry text COLLATE pg_catalog."default",
+    address text COLLATE pg_catalog."default",
+    valuation_value text COLLATE pg_catalog."default",
+    valuation_date text COLLATE pg_catalog."default"
+)
+
+CREATE TABLE cadnumtemp (cad TEXT);
+
+CREATE TABLE cadnumerror (cader TEXT);
+
+ALTER TABLE cadnum ADD CONSTRAINT unique_cad UNIQUE (cad);
