d9558d62e78f8647e6ec09f7630e852d4b8bfdba
Bonse <84657555+lxthxrgx@users.noreply.github.com>
2023-07-10 14:08:28 +0300

    Update data_base.sql

--- data_base.sql
+++ data_base.sql
@@ -18,6 +18,25 @@ CREATE TABLE IF NOT EXISTS public.info_about_cadnum
 	geometry text COLLATE pg_catalog."default"
 )
 
+CREATE TABLE IF NOT EXIST data_test
+(
+    cadnum text COLLATE pg_catalog."default",
+    category text COLLATE pg_catalog."default",
+    area text COLLATE pg_catalog."default",
+    unit_area text COLLATE pg_catalog."default",
+    koatuu text COLLATE pg_catalog."default",
+    use text COLLATE pg_catalog."default",
+    purpose text COLLATE pg_catalog."default",
+    purpose_code text COLLATE pg_catalog."default",
+    ownership text COLLATE pg_catalog."default",
+    ownershipcode text COLLATE pg_catalog."default",
+    geom GEOMETRY(MultiPolygon,4326),
+    address text COLLATE pg_catalog."default",
+    valuation_value text COLLATE pg_catalog."default",
+    valuation_date text COLLATE pg_catalog."default",
+	geometry text COLLATE pg_catalog."default"
+)
+	
 CREATE TABLE cadnumtemp (cad TEXT);
 
 CREATE TABLE cadnumerror (cader TEXT);
